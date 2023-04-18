84296b5d0080958a07cc9b08e324e1026505e035
Oleh Savaryn <olegsavaryn@gmail.com>
2023-04-18 19:23:41 +0200

    hw3

--- validators/validateData.js
+++ validators/validateData.js
@@ -1,7 +1,5 @@
 const Joi = require("joi");
-const fs = require("fs/promises");
-const path = require("path");
-const contactsPath = path.join("models", "contacts.json");
+const mongoose = require("mongoose");
 
 const contactValidatePost = Joi.object({
   name: Joi.string().required(),
@@ -17,11 +15,11 @@ const contactValidatePut = Joi.object({
 
 async function validateGetById(req, res, next) {
   const { contactId } = req.params;
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const contacts = JSON.parse(data).filter((elem) => elem.id === contactId);
-  if (contacts.length === 0) {
-    res.status(404).json({ message: "Not found" });
-  } else next();
+
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return res.status(400).json({ message: "Not found" });
+  }
+  next();
 }
 
 function validatePost(req, res, next) {
@@ -55,12 +53,9 @@ function validatePost(req, res, next) {
 
 async function validateDelete(req, res, next) {
   const { contactId } = req.params;
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const contacts = JSON.parse(data);
-  const contactToDel = contacts.find((elem) => elem.id === contactId);
-  if (!contactToDel) {
-    res.status(404).json({ message: "Not found" });
-    return;
+
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return res.status(400).json({ message: "Not found" });
   }
   next();
 }
@@ -68,11 +63,8 @@ async function validateDelete(req, res, next) {
 async function validateUpdate(req, res, next) {
   const body = req.body;
   const { contactId } = req.params;
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const contacts = JSON.parse(data).filter((elem) => elem.id === contactId);
-  if (contacts.length === 0) {
-    res.status(404).json({ message: "Not found " });
-    return;
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return res.status(400).json({ message: "Not found" });
   }
   if (!body.name && !body.email) {
     res.status(400).json({ message: "missing fields" });
@@ -88,9 +80,22 @@ async function validateUpdate(req, res, next) {
   next();
 }
 
+async function validateFavorite(req, res, next) {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  if (!favorite) {
+    return res.status(400).json({ message: "missing field favorite" });
+  }
+  next();
+}
+
 module.exports = {
   validateGetById,
   validatePost,
   validateDelete,
   validateUpdate,
+  validateFavorite,
 };
